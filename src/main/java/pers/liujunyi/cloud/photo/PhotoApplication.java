6c5ab809a248b8b270c3d092c659c7f41c78b6a9
lbyjwwyqt147 <lbyjwwyqt147@163.com>
2019-02-19 15:38:52 +0800

    初始化

---
+++ src/main/java/pers/liujunyi/cloud/photo/PhotoApplication.java
@@ -0,0 +1,13 @@
+package pers.liujunyi.cloud.photo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoApplication.class, args);
+    }
+
+}
